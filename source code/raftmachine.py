e06fda82ff18ba8c423a286907291b70ad447013
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2019-07-15 21:57:56 +0800

    initial commit

---
+++ source code/raftmachine.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+__author__ = 'DozingWolf'
+import sys
+
+class RaftMachine(Base):
+    print('Raft-Python Machine')
+    def __init__():
+        ***
+        加载配置文件
+        ***
+        pass
+    def RaftMachineInitial():
+        ***
+        Raft算法初始化
+        ***
+        pass
+    def RaftMachineCharacterSwitch():
+        ***
+        Raft角色切换
+        ***
+        pass
+    def getRaftMachineInfo():
+        ***
+        获取raft机器运行状态信息
+        ***
+        pass
