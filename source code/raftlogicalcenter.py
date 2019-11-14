161fc118df82163fbf0f4db0e5e38c7a7a717518
DozingWolf <43899290+DozingWolf@users.noreply.github.com>
2019-11-14 22:50:01 +0800

    add inherit class

--- source code/raftlogicalcenter.py
+++ source code/raftlogicalcenter.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'DozingWolf'
+
+from raftmachine import RaftMachine
+
+class RaftLogicalCenter(RaftMachine):
+    def __init__(self):
+        super().__init__(modelname = 'RaftLogicalCenter')
+        self.__logicalCenterLogger = super().getRMLogger()
+    def getLogicalCenterInfo(self):
+        pass
